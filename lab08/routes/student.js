9ac172f2afcf3f77aef6aec1cd43f8667134cebc
Nguyen Dinh Tran Long <longndt226@gmail.com>
2023-08-08 10:35:05 +0700

    lab8

--- lab08/routes/student.js
+++ lab08/routes/student.js
@@ -4,8 +4,25 @@ var router = express.Router();
 
 router.get('/', async (req, res) => {
    var students = await StudentModel.find();
-   //res.send(students);
    res.render('student/studentList', { students : students });
 });
+
+router.get('/delete/:id', async (req, res) => {
+   // var id = req.params.id;
+   // await StudentModel.findByIdAndDelete(id)
+   // .then(() => console.log ("Delete successfully !"))
+   // .catch(err => console.log("Delete failed !")); 
+   // res.redirect('/student');
+   await StudentModel.findByIdAndDelete(req.params.id);
+   res.redirect('/student');
+})
+
+router.get('/add', (req, res) => {
+   res.render('student/studentAdd');
+})
+
+router.post('/add', async (req, res) => {
+   
+})
 
 module.exports = router;
